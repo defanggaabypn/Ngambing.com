bc571da745ad8fe4c1e1c2bfff26083685105592
defanggaabypn <onecircle.24@gmail.com>
2020-01-23 01:59:09 +0700

    test

---
+++ app/Http/Middleware/CustomerAuthenticate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class CustomerAuthenticate
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        //JADI KITA CEK, JIKA GUARD CUSTOMER BELUM LOGIN
+        if (!auth()->guard('customer')->check()) {
+            //MAKA REDIRECT KE HALAMAN LOGIN
+            return redirect(route('customer.login'));
+        }
+        return $next($request);
+    }
+}
